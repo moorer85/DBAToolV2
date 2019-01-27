a72f8bf469f2a9fa06ad80d69d95c4a20e168b39
Roy L Moore <roy.l.moore@hotmail.com>
2019-01-27 07:48:26 -0500

    changed startup

--- DBAToolV2/Startup.cs
+++ DBAToolV2/Startup.cs
@@ -69,7 +69,7 @@ namespace DBAToolV2
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Databases}/{action=Index}/{id?}");
+                    template: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
